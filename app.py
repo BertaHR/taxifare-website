68c46f7b31536ea0a33dcc2af246c9db714d8982
Liubov Zakharchenko <bertahr36@gmail.com>
2025-03-07 15:08:09 +0100

    second

--- app.py
+++ app.py
@@ -30,6 +30,8 @@ params = {
     "passenger_count": passenger_count
 }
 
+url = 'https://taxifare.lewagon.ai/predict'
+
 # 3️⃣ API call button
 if st.button("📡 Get Fare Prediction"):
     try:
@@ -44,59 +46,3 @@ if st.button("📡 Get Fare Prediction"):
 
 # Final message
 st.markdown("🚀 Try entering different values and get your fare prediction!")
-
-
-# pickup_datetime: str,  # 2014-07-06 19:18:00
-        # pickup_longitude: float,    # -73.950655
-        # pickup_latitude: float,     # 40.783282
-        # dropoff_longitude: float,   # -73.984365
-        # dropoff_latitude: float,    # 40.769802
-        # passenger_count: 
-'''
-# TaxiFareModel front
-'''
-
-st.markdown('''
-Remember that there are several ways to output content into your web page...
-
-Either as with the title by just creating a string (or an f-string). Or as with this paragraph using the `st.` functions
-''')
-
-
-
-'''
-## Here we would like to add some controllers in order to ask the user to select the parameters of the ride
-
-1. Let's ask for:
-- date and time
-- pickup longitude
-- pickup latitude
-- dropoff longitude
-- dropoff latitude
-- passenger count
-'''
-
-'''
-## Once we have these, let's call our API in order to retrieve a prediction
-
-See ? No need to load a `model.joblib` file in this app, we do not even need to know anything about Data Science in order to retrieve a prediction...
-
-🤔 How could we call our API ? Off course... The `requests` package 💡
-'''
-
-url = 'https://taxifare.lewagon.ai/predict'
-
-if url == 'https://taxifare.lewagon.ai/predict':
-
-    st.markdown('Maybe you want to use your own API for the prediction, not the one provided by Le Wagon...')
-
-'''
-
-2. Let's build a dictionary containing the parameters for our API...
-
-3. Let's call our API using the `requests` package...
-
-4. Let's retrieve the prediction from the **JSON** returned by the API...
-
-## Finally, we can display the prediction to the user
-'''
